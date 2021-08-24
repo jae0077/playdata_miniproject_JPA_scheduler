bb8afcbc404a2c22c34c08e3ef81fabc9df137a1
jh <sjj0077@naver.com>
2021-08-24 09:50:23 +0900

    [Add] Controller

---
+++ src/scheduler/controller/SchedulerController.java
@@ -0,0 +1,24 @@
+package scheduler.controller;
+
+import scheduler.model.ParticipantDAO;
+import scheduler.model.SchedulerDAO;
+import scheduler.model.MemberDAO;
+
+public class SchedulerController {
+	private static SchedulerController instance = new SchedulerController();
+	private SchedulerController() {}
+	public static SchedulerController getInstance() {
+		return instance;
+	}
+	
+	// static 강사님께 여쭤보기
+	MemberDAO memberDAO = MemberDAO.getInstance();
+	SchedulerDAO schedulerDAO = SchedulerDAO.getInstance();
+	ParticipantDAO participantDAO = ParticipantDAO.getInstance();
+	
+	public void register(String id, String pw, String name, String phone) {
+		memberDAO.memberRegister(id, pw, name, phone);
+
+	}
+	
+}
